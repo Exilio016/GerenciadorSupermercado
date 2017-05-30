d4d48f2ef6c7b82b05bec78ce918c4912c781192
Bruno Flávio (Exílio) <ndninguem9@gmail.com>
2017-05-30 11:09:36 -0300

    Tabelas cliente e funcionario atualizados

--- src/main/java/gerenciamentoMercado/bancoDeDados/BancoDeDados.java
+++ src/main/java/gerenciamentoMercado/bancoDeDados/BancoDeDados.java
@@ -16,35 +16,35 @@ public class BancoDeDados {
 
         //Se o banco de dados estiver vazio, cria-se as tabelas do programa
         try{
-            String sql = "CREATE TABLE Funcionarios (cpf VARCHAR(11) NOT NULL, nome VARCHAR(32), cargo VARCHAR(32) NOT NULL, salario FLOAT, primary key (cpf) )";
+            String sql = "CREATE TABLE Funcionario (nome VARCHAR(50), cpf VARCHAR(11) NOT NULL, rg VARCHAR(15), rua VARCHAR(50), cep INT(8), bairro VARCHAR(25), cidade VARCHAR(25), estado VARCHAR(25), numero INT, complemento VARCHAR(25), telefone INT(11), celular INT(12), salario FLOAT, cargo INT, primary key (cpf) )";
 
-            stmt = conn.prepareStatement(sql); //Se não existir, cria a tabela Funcionarios
+            stmt = conn.prepareStatement(sql); //Se nao existir, cria a tabela Funcionarios
             stmt.execute();
             stmt.close();
         } catch(SQLException se){
-            //Se deu exceção, a tabela já existe no banco
+            //Se deu excecao, a tabela ja existe no banco
         }
 
         try{
-            String sql = "CREATE TABLE Clientes (cpf VARCHAR(11) NOT NULL, nome VARCHAR(32), tipo VARCHAR(32) NOT NULL, primary key (cpf) )";
+            String sql = "CREATE TABLE Cliente (nome VARCHAR(32), cpf VARCHAR(11) NOT NULL, rg VARCHAR(15), rua VARCHAR(50), cep INT(8), bairro VARCHAR(25), cidade VARCHAR(25), estado VARCHAR(25), numero INT, complemento VARCHAR(25), telefone INT(11), celular INT(12), cartao INT(16), primary key (cpf) )";
 
-            stmt = conn.prepareStatement(sql); //Se não existir, cria a tabela clientes
+            stmt = conn.prepareStatement(sql); //Se nao existir, cria a tabela clientes
             stmt.execute();
             stmt.close();
         } catch(SQLException se){
-            //Se deu exceção, a tabela já existe no banco
+            //Se deu excecao, a tabela ja existe no banco
         }
 
 
         //Se o banco de dados estiver vazio, cria-se as tabelas do programa
         try{
-            String sql = "CREATE TABLE Produtos (codigo BIGINT NOT NULL, descricao VARCHAR(255), valorunit FLOAT, quantidade INT, primary key (codigo) )";
+            String sql = "CREATE TABLE Produto (codigo BIGINT NOT NULL, descricao VARCHAR(255), valorunit FLOAT, quantidade INT, primary key (codigo) )";
 
-            stmt = conn.prepareStatement(sql);//Se não existir, cria a tabela produtos
+            stmt = conn.prepareStatement(sql);//Se nao existir, cria a tabela produtos
             stmt.execute();
             stmt.close();
         } catch(SQLException se){
-            //Se deu exceção, a tabela já existe no banco
+            //Se deu excecao, a tabela ja existe no banco
         }
     }
 }

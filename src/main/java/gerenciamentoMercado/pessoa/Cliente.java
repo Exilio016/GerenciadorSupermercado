e7f42a28d07113a9ea2026c4175008e83ba4c126
Bruno Flavio <bruno.flavio.ferreira@usp.br>
2017-06-01 18:16:09 -0300

    Projeto estruturado

--- src/main/java/gerenciamentoMercado/pessoa/Cliente.java
+++ src/main/java/gerenciamentoMercado/pessoa/Cliente.java
@@ -1,5 +1,8 @@
 package gerenciamentoMercado.pessoa;
 
 public class Cliente extends Pessoa{
-	
+
+    public Cliente(String estado, String cidade, String bairro, String rua, int numero, String complemento, String CEP, String CPF, String RG, String telefone, String celular) {
+        super(estado, cidade, bairro, rua, numero, complemento, CEP, CPF, RG, telefone, celular);
+    }
 }
---
+++ src/test/java/gerenciamentoMercado/bancoDeDados/BancoDeDadosTest.java
@@ -0,0 +1,19 @@
+package gerenciamentoMercado.bancoDeDados;
+
+import org.junit.Test;
+
+import java.sql.SQLException;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by nding on 01/06/2017.
+ */
+public class BancoDeDadosTest {
+
+    @Test
+    public void BancoDeDados() throws SQLException{
+        BancoDeDados bd = new BancoDeDados();
+    }
+
+}

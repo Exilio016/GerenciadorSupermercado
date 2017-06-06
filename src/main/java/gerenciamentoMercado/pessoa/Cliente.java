cc87ce10d9e0e4f62d576ee01dbf6033613359d9
Edson Y. Toma <edson.toma@usp.br>
2017-06-06 10:38:18 -0300

    Desconto padrao de 10%

--- src/main/java/gerenciamentoMercado/pessoa/Cliente.java
+++ src/main/java/gerenciamentoMercado/pessoa/Cliente.java
@@ -2,7 +2,7 @@ package gerenciamentoMercado.pessoa;
 
 public class Cliente extends Pessoa{
     String cartao;
-    public static float desconto;
+    public static float desconto = 0.1f;
 
     public Cliente(String estado, String cidade, String bairro, String rua, int numero, String complemento, String CEP, String CPF, String RG, String telefone, String celular, String cartao, String nome) {
         super(estado, cidade, bairro, rua, numero, complemento, CEP, CPF, RG, telefone, celular, nome);

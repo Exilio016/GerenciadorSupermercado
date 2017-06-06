5f8f9e504790a023bd3a29a183c6644c43c1ce4d
Bruno Flávio (Exílio) <ndninguem9@gmail.com>
2017-06-06 10:39:52 -0300

    GUI modificada

--- src/main/java/gerenciamentoMercado/gui/LoginGUI.java
+++ src/main/java/gerenciamentoMercado/gui/LoginGUI.java
@@ -3,25 +3,21 @@ package gerenciamentoMercado.gui;
 import javax.swing.*;
 import java.awt.*;
 
-/**
- * Created by nding on 05/06/2017.
- */
 public class LoginGUI extends JPanel {
     private JTextField usuario = new JTextField();
     private JPasswordField senha = new JPasswordField();
     private JButton login = new JButton("Login");
-    private JButton cadastar = new JButton("Cadastrar");
 
     public LoginGUI(MainGUI frame){
         login.setActionCommand("LOGIN");
         login.addActionListener(frame);
-
+        
         usuario.setPreferredSize(new Dimension(300, 30));
         senha.setPreferredSize(new Dimension(300, 30));
 
         JPanel usuarioPanel = new JPanel();
         usuarioPanel.setLayout(new FlowLayout());
-        usuarioPanel.add(new JLabel("Usu√°rio: "));
+        usuarioPanel.add(new JLabel("Usu·rio: "));
         usuarioPanel.add(usuario);
 
         JPanel senhaPanel = new JPanel();
@@ -32,12 +28,12 @@ public class LoginGUI extends JPanel {
         JPanel loginPanel = new JPanel();
         loginPanel.setLayout(new FlowLayout());
         loginPanel.add(login);
-        loginPanel.add(cadastar);
 
         this.setLayout(new GridLayout(3, 1));
         this.add(usuarioPanel);
         this.add(senhaPanel);
         this.add(loginPanel);
+       
     }
 
     public JTextField getUsuario(){
--- src/main/java/gerenciamentoMercado/gui/MainGUI.java
+++ src/main/java/gerenciamentoMercado/gui/MainGUI.java
@@ -8,9 +8,6 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.sql.SQLException;
 
-/**
- * Created by nding on 05/06/2017.
- */
 public class MainGUI extends JFrame implements ActionListener{
     BancoDeDados bd;
     Dimension screenSize;
@@ -20,7 +17,6 @@ public class MainGUI extends JFrame implements ActionListener{
         this.setSize(400,400);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setContentPane(new LoginGUI(this));
-
         this.bd = bd;
     }
 
@@ -31,17 +27,21 @@ public class MainGUI extends JFrame implements ActionListener{
 
     public void actionPerformed(ActionEvent e) {
         if(e.getActionCommand().equals("LOGIN")){
-            String usuario = ((LoginGUI) this.getContentPane()).getUsuario().getText();
-            String senha = new String(((LoginGUI) this.getContentPane()).getSenha().getPassword());
+        	
+            JTextField usuario = ((LoginGUI) this.getContentPane()).getUsuario();
+            JPasswordField senha = ((LoginGUI) this.getContentPane()).getSenha();
 
-            if(bd.logar(usuario, senha)){
-                //TODO trocar para janela de controle do caixa
-                this.setSize(screenSize);
+            if(bd.logar(usuario.getText(), new String(senha.getPassword()))){
+                this.setLocation(0, 0); //Centraliza a GUI
+                this.setSize(screenSize); //Aumenta o tamanho para a proxima GUI
                 System.out.println("Login realizado com sucesso");
+                
+                //TODO trocar para janela de controle do caixa
             }
-
-        }
-        else if(e.getActionCommand().equals("CADASTRAR")){
+            else {
+            	JOptionPane.showMessageDialog(this, "Erro ao realizar o login! \nUsuario ou senha incorretos");
+            	senha.setText("");
+            }
 
         }
     }
